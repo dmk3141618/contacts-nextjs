3dd91dadcca6dd65cacff5da4cf55bec784496cb
dmk3141618 <dmk3141618@gmail.com>
2021-09-25 13:23:42 +0900

    feat: apply absolute path and update directory structure with src

---
+++ src/pages/common/component/TestComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TestComponent() {
+  return <div>test component</div>;
+}
+
+export default TestComponent;
---
+++ src/pages/test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import TestComponent from './common/component/TestComponent';
+
+function TestPage() {
+  return <TestComponent />;
+}
+
+export default TestPage;
